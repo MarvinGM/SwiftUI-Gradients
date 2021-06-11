5bd8441241c6d50f5e96490f4192f5902e6403c9
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-11 13:07:10 +0800

    Initial Commit

---
+++ SwiftUI Gradients/SwiftUI_GradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GradientsApp.swift
+//  SwiftUI Gradients
+//
+//  Created by Amben on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
